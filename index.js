c3519657b5d1a4e254885dca5d25c6ce76b4f17c
Muhhae <haekal.muhamat@gmail.com>
2023-06-19 06:01:20 +0700

    Fetching Pokemon Data

---
+++ index.js
@@ -0,0 +1,21 @@
+const PokeApi = "https://pokeapi.co/api/v2/pokemon";
+
+const fetchData = async (apiEndPoint, pokemon) => {
+    console.log("Fetching API for ", pokemon);
+    try {
+        const response = await fetch(apiEndPoint + "/" + pokemon);
+        const jsonData = await response.json();
+        console.log("Finish Fetching API for Pokemon ", pokemon, "\n", jsonData);
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+
+let poke = ["ditto", "pikachu", "rando", "bulbasaur"];
+
+poke.forEach(pokemon => {
+    console.log("Try to fetch data for ", pokemon);
+    fetchData(PokeApi, pokemon);
+    console.log("Still in process fetching ", pokemon);
+})
